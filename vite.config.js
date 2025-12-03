bd472269b0fdb1927bcfd9f66cf089f04000c0ad
Scott Petrovic <scottpetrovic@gmail.com>
2025-12-02 21:04:59 -0600

    Add retarget file to vite so it includes it in final build

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,8 @@ export default {
       rollupOptions: {
         input: {
           main: resolve(__dirname, 'src/index.html'),
-          create: resolve(__dirname, 'src/create.html')
+          create: resolve(__dirname, 'src/create.html'),
+          retarget: resolve(__dirname, 'src/retarget/index.html')
         }
       }
     },
